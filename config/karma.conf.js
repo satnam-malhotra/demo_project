694471180575f28fd9f6fa1f21e47f37948dad04
Satnam Malhotra <satnam.malhotra@3pillarglobal.com>
2020-05-01 23:36:19 +0530

    Testing, master

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function (config) {
         },
 
 
-        // test results reporter to use
+        // test results reporter to user
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
         reporters: ['progress', 'kjhtml'],
